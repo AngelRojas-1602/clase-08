3bcf6221ed374e1bfdfd71f61c2f786b0fd1071d
AngelRojas-1602 <angelnest1602@live.com.mx>
2020-12-10 23:03:54 -0600

    Primer Commit

---
+++ src/components/Ejemplo2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+import {Container} from '@material-ui/core'
+import Formulario from './Formulario'
+
+const Ejemplo2 = () => {
+  return (
+    <div>
+      <Header />
+      <Container>
+        <Formulario />
+      </Container>
+    </div>
+  )
+}
+
+export default Ejemplo2
